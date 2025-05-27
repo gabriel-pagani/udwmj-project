d868172a64901d5f83c7e5f32355031e40f52ae2
gabriel-pagani <gabrielpaganidesouza@gmail.com>
2025-05-26 23:07:57 -0300

    Adicionei contexto para a view home e criei um arquivo parcial dos cards de receita

--- app/views.py
+++ app/views.py
@@ -1,4 +1,22 @@
 from django.shortcuts import render
+from app.models import Recipe
+
 
 def home(request):
-  return render(request, 'app/index.html')
+    recipes = dict()
+    for recipe in Recipe.objects.all():
+        recipes[f'{recipe.id}'] = [
+            recipe.title,
+            recipe.description,
+            recipe.instructions,
+            recipe.preparation_time,
+            recipe.servings,
+            recipe.cover,
+            recipe.category.name if recipe.category else None,
+            recipe.author.username if recipe.author else None,
+            recipe.updated_at,
+            recipe.created_at,
+            recipe.is_published,
+        ]
+
+    return render(request, 'app/index.html', {'recipes': recipes})
